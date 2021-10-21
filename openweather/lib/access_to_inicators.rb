0c15a306093a44e4d52700d0d04a65a64d17ab6e
Ilya <lyamsh@yandex.ru>
2021-10-21 03:02:52 +0300

    feat: solve weather task

---
+++ openweather/lib/access_to_inicators.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+require_relative 'convertor'
+
+module AccessToIndicators
+  def data_daily_dif_tempeture(data)
+    acc = []
+    fiveday = gets_tempeture_five_night(data)
+    fiveday.each_pair do |key, value|
+      acc.push(key) if value == (fiveday.values.min)
+    end
+    {days: acc, diff_tempeture: fiveday.values.min}
+  end
+
+  def gets_tempeture_five_night(data)
+    data_daily_dif = {}
+    data['daily'].take(5).each do |day|
+      data_daily_dif[(day['dt'])] =
+        (day['temp']['night'] - day['feels_like']['night']).abs
+    end
+    data_daily_dif
+  end
+
+
+
+  def data_daily_light(data)
+    data_daily_dif = {}
+    acc = []
+    data['daily'].take(5).each do |day|
+      data_daily_dif[(day['dt'])] =
+      (Convertor.data_from_unix(day['sunset']) - Convertor.data_from_unix(day['sunrise'])).abs
+    end
+    data_daily_dif.each_pair do |key, value|
+      acc.push(key) if value == (data_daily_dif.values.max)
+    end
+    {days: acc, daylight_minutes: (data_daily_dif.values.max/60.0)}
+  end
+
+
+  def gets_days(data)
+    acc = []
+    data['daily'].each { |day| acc << day['dt'] }
+    acc
+  end
+
+  def current_day(data)
+    data['current']['dt']
+  end
+end
---
+++ openweather/weather.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require_relative 'lib/access_to_inicators'
+require_relative 'lib/convertor'
+require_relative 'lib/requestweather'
+
+class Task
+  include AccessToIndicators
+
+  def initialize
+    @data = RequestWeather.gets_data
+  end
+
+  def gets_day_with_max_diff_feeling_from_real_in_night
+        answer = data_daily_dif_tempeture(@data)
+    days, diff_tempeture = answer[:days], answer[:diff_tempeture]
+    "#{Convertor.many_date_from_unix(days)} числа разница между ночной температурой и её ощущением будет минимальна в ближайщие 5 дней: #{diff_tempeture} градуса по Цельсию"
+  end
+
+
+  def gets_day_with_max_light
+    answer = data_daily_light(@data)
+    daylight_minutes, days = answer[:daylight_minutes], answer[:days]
+    "The longest days: #{Convertor.many_date_from_unix(days)}. Duration: #{daylight_minutes}"
+  end
+end
+
+a = Task.new
+puts a.gets_day_with_max_diff_feeling_from_real_in_night
+puts a.gets_day_with_max_light
