1216e50060847e76145194f1c36daeb426678786
Ilya <lyamsh@yandex.ru>
2021-10-24 00:43:49 +0300

    fix: change in module : extend self

--- openweather/lib/convertor.rb
+++ openweather/lib/convertor.rb
@@ -1,11 +1,12 @@
 require 'time'
 
 module Convertor
-  def self.data_from_unix(date)
+  extend self
+  def data_from_unix(date)
     Time.strptime(date.to_s, '%s')
   end
 
-  def self.many_date_from_unix(arr_date)
+  def many_date_from_unix(arr_date)
     arr_date.map{ |date| (data_from_unix(date)).strftime("%d/%m/%Y")  }.join(', ')
   end
 
