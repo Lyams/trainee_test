5c7d187028b12aa8ff917c4ff6491ba02cca820f
Ilya <lyamsh@yandex.ru>
2021-10-25 05:04:49 +0300

    test: add tests for hash_values and text output

--- openweather/lib/access_to_inicators.rb
+++ openweather/lib/access_to_inicators.rb
@@ -8,7 +8,7 @@ module AccessToIndicators
     fiveday.each_pair do |key, value|
       acc.push(key) if value == (fiveday.values.min)
     end
-    {days: acc, diff_tempeture: fiveday.values.min}
+    { days: acc, diff_temperature: fiveday.values.min }
   end
 
   def gets_temperature_five_night(data)
@@ -20,21 +20,18 @@ module AccessToIndicators
     data_daily_dif
   end
 
-
-
   def data_daily_light(data)
     data_daily_dif = {}
     acc = []
     data['daily'].take(5).each do |day|
       data_daily_dif[(day['dt'])] =
-      (Convertor.data_from_unix(day['sunset']) - Convertor.data_from_unix(day['sunrise'])).abs
+        (Convertor.data_from_unix(day['sunset']) - Convertor.data_from_unix(day['sunrise'])).abs
     end
     data_daily_dif.each_pair do |key, value|
       acc.push(key) if value == (data_daily_dif.values.max)
     end
-    {days: acc, daylight_minutes: (data_daily_dif.values.max/60.0)}
+    { days: acc, daylight_minutes: (data_daily_dif.values.max / 60.0) }
   end
-
 
   def gets_days(data)
     acc = []
--- openweather/weather.rb
+++ openweather/weather.rb
@@ -6,17 +6,17 @@ require_relative 'lib/requestweather'
 
 class Task
   include AccessToIndicators
+  attr_reader :data
 
   def initialize
     @data = RequestWeather.gets_data
   end
 
   def gets_day_with_min_diff_feeling_from_real_in_night
-        answer = data_daily_dif_temperature(@data)
-    days, diff_temperature = answer[:days], answer[:diff_tempeture]
+    answer = data_daily_dif_temperature(@data)
+    days, diff_temperature = answer[:days], answer[:diff_temperature]
     "#{Convertor.many_date_from_unix(days)} числа разница между ночной температурой и её ощущением будет минимальна в ближайщие 5 дней: #{diff_temperature } градуса по Цельсию"
   end
-
 
   def gets_day_with_max_light
     answer = data_daily_light(@data)
